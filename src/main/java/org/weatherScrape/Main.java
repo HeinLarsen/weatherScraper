ba905b3150f63fd32def3102bd4402f15ef9f26c
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2023-09-25 17:53:45 +0200

    EER-Diagram

--- src/main/java/org/weatherScrape/Main.java
+++ src/main/java/org/weatherScrape/Main.java
@@ -6,5 +6,6 @@ import org.weatherScrape.config.HibernateConfig;
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig();
+
     }
 }
