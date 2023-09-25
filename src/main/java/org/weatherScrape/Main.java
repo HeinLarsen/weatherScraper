8e2434ca627c814fcb91924fd22af1a0d21e3fb2
Tobias Tonndorff <tobias@tonndorff.dk>
2023-09-25 11:32:09 +0200

    testing workflow

--- src/main/java/org/weatherScrape/Main.java
+++ src/main/java/org/weatherScrape/Main.java
@@ -3,5 +3,7 @@ package org.weatherScrape;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("testing workflow");
+
     }
 }
