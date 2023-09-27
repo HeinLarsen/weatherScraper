cd48158cc15d37f7641085a49028a53512f79c06
Anders Hein Larsen <andersheinlarsen@gmail.com>
2023-09-27 12:28:19 +0200

    fix scrape

--- src/main/java/org/weatherScrape/helper/Helpers.java
+++ src/main/java/org/weatherScrape/helper/Helpers.java
@@ -2,10 +2,12 @@ package org.weatherScrape.helper;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Helpers {
 
     public static LocalTime getLocalTime(String time) {
+        Locale.setDefault(Locale.US);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a");
         return LocalTime.parse(time, formatter);
     }
--- src/main/java/org/weatherScrape/util/Scraper.java
+++ src/main/java/org/weatherScrape/util/Scraper.java
@@ -33,7 +33,7 @@ public class Scraper {
     public static List<Region> getRegions(Document doc) {
         List<Region> regions = new ArrayList<>();
         // tmp hardcode to Capital Region of Denmark
-        Elements elements = doc.select("body > div > div.two-column-page-content > div.page-column-1 > div.page-content.content-module > div.result-container > a");
+        Elements elements = doc.select("body > div > div.two-column-page-content > div.page-column-1 > div.page-content.content-module > div.result-container > a:nth-child(1)");
         Elements country = doc.getElementsByClass("location-title");
 
 
@@ -62,7 +62,7 @@ public class Scraper {
                         System.out.println("Fetching cities for " + region.getName() + " on thread " + Thread.currentThread().getName());
                         List<City> regionCities = new ArrayList<>();
                         Document doc = fetchData(baseUrl + region.getCountryCode() + "/" + region.getId());
-                        Elements aCities = doc.select("body > div > div.two-column-page-content > div.page-column-1 > div.page-content.content-module > div.result-container > a:nth-child(1)");
+                        Elements aCities = doc.select("body > div > div.two-column-page-content > div.page-column-1 > div.page-content.content-module > div.result-container > a");
                         for (Element aCity : aCities) {
                             String name = aCity.text();
                             String str = aCity.attr("href");
